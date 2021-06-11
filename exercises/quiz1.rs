736698bd9667e206c305a2107deed64e03b72d40
Sudeep Kumar <sudeepdino008@gmail.com>
2021-06-11 16:38:06 +0530

    solutions to rustlings exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -7,8 +7,6 @@
 // more than 40 at once, each apple only costs 1! Write a function that calculates
 // the price of an order of apples given the order amount. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
 // fn ..... {
 
@@ -22,4 +20,8 @@ fn verify_test() {
     assert_eq!(70, price1);
     assert_eq!(80, price2);
     assert_eq!(65, price3);
+}
+
+fn calculate_apple_price(napples: i32) -> i32 {
+    if napples>40 { napples } else { napples * 2 }
 }
